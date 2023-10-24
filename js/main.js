9a29621c8fb0eb7414883f80ecd05fec47d830a1
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-10-24 12:26:31 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const overlay = document.querySelector('.card__image_overlay')
+document.querySelector('.card__image').addEventListener('mouseenter', () => {
+    overlay.style.display = 'flex'
+})
+document.querySelector('.card__image').addEventListener('mouseleave', () => {
+    overlay.style.display = 'none'
+})
 
